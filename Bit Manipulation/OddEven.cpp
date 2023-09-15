815a2a2f6f5286f80ece245383dfb371d6de983c
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-09-15 14:46:37 +0530

    Create OddEven.cpp

---
+++ Bit Manipulation/OddEven.cpp
@@ -0,0 +1,8 @@
+// https://www.codingninjas.com/studio/problems/odd-even_7993579?leftPanelTab=0
+
+string oddEven(int n){
+    //int t=1;
+    if((1 & n)==0)
+        return "even";
+    return "odd";
+}
