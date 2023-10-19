c3e9faf9c7909fc4bf1483bbda62f30de8127024
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2023-10-19 22:10:17 +0530

    Create LevelOrderTraversal.cpp

---
+++ Tree/LevelOrderTraversal.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)
+            return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        vector<int>cur;
+        while(!q.empty())
+        {
+            TreeNode* t=q.front();
+            q.pop();
+            if(t==NULL)
+            {
+                if(q.size()>0)
+                    q.push(NULL);
+                ans.push_back(cur);
+                cur.resize(0);
+            }
+            else 
+            {
+                cur.push_back(t->val);
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
+            }
+        }
+        return ans;
+    }
+};
