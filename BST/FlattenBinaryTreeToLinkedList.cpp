5ee61c07280a6917328a8c3a0526d4c5b46f8466
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2024-02-25 11:16:58 +0530

    Create FlattenBinaryTreeToLinkedList.cpp

---
+++ BST/FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root==NULL)
+            return ;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty())
+        {
+            auto it=st.top();
+            st.pop();
+            TreeNode* node=it;
+            if(node->right)
+                st.push(node->right);
+            if(node->left)
+                st.push(node->left);
+            if(!st.empty())
+                node->right=st.top();
+            node->left=NULL;
+        }
+    }
+};
