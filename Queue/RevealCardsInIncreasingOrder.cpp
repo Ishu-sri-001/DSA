ea948c6f034074c507bba36ed76fbdc40f6dbd12
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2024-04-10 18:30:35 +0530

    Create RevealCardsInIncreasingOrder.cpp

---
+++ Queue/RevealCardsInIncreasingOrder.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reveal-cards-in-increasing-order/description/
+
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        queue<int>q;
+        sort(deck.begin(),deck.end());
+        for(int i=deck.size()-1;i>=0;i--)
+        {
+            if(!q.empty())
+            {
+                q.push(q.front());
+                q.pop();
+            }
+            q.push(deck[i]);
+        }
+        for(int i=deck.size()-1;i>=0;i--)
+        {
+            deck[i]=q.front();
+            q.pop();
+        }
+        return deck;
+    }
+};
