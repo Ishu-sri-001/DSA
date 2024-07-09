d389149155696cb4b9c177043bc25b6961b7d198
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2024-07-09 10:59:26 +0530

    Create AvgWaitingTime.cpp

---
+++ Matrice/AvgWaitingTime.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/average-waiting-time/description/?envType=daily-question&envId=2024-07-09
+
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& c) {
+        long res=0;
+        int time=c[0][0];
+        int len=c.size();
+        for (int i=0;i<len;i++)
+        {
+            time=max(time,c[i][0]);
+            time+=c[i][1];
+            res+=(time-c[i][0]);
+        }
+        double ans=(double)res/len;
+        return ans;
+    }
+};
